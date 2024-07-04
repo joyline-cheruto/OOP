468cbc5431fb9e7ebd6fd78765e2726d3ee2e08a
joyline-cheruto <joycheruto832@gmail.com>
2024-07-04 16:16:31 +0300

    introduction of methods

--- get.py
+++ get.py
@@ -1,14 +1,18 @@
 class Staff:
-   ...
+    def __init__(self, name, department):
+        self.name = ""
+        self.department = ""
+
+
 def main():
     staff = get_staff()
     print(f"{staff.name} from {staff.department}")
 
 
 def get_staff():
-    staff = Staff()
-    staff.name = input("Enter name: ")
-    staff.department = input("Enter department: ")
+    name = input("Enter name: ")
+    department = input("Enter department: ")
+    staff = Staff(name, department)
     return staff
 
 
