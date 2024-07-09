9a966de0f784fcdd29c1b1dbaf2aa47490c98fe4
joyline-cheruto <joycheruto832@gmail.com>
2024-07-09 16:57:57 +0300

    code refactor

--- codecamp.py
+++ codecamp.py
@@ -1,15 +1,14 @@
 class Item:
-    def __init__(self,name):
+    def __init__(self, name, price, quantity):
         print(f"Instance created: {name}")
         self.name = name
+        self.price = price
+        self.quantity = quantity
+
     def calculate_total_price(self, x, y):
         return x * y
 
 
-item1 = Item('phone')
-item1.price = 1200
-item1.quantity = 5
+item1 = Item('phone', 1200, 5)
 
-item1 = Item('Laptop')
-item1.price = 1200
-item1.quantity = 5
+item1 = Item('Laptop', 2000, 3)
