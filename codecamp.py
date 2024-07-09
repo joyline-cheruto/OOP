f180900bd0877145ce431cf43c1c5c3113e85340
joyline-cheruto <joycheruto832@gmail.com>
2024-07-09 16:53:25 +0300

    __init__

--- codecamp.py
+++ codecamp.py
@@ -1,10 +1,15 @@
 class Item:
+    def __init__(self,name):
+        print(f"Instance created: {name}")
+        self.name = name
     def calculate_total_price(self, x, y):
         return x * y
 
 
-item1 = Item()
-item1.name = 'phone'
+item1 = Item('phone')
 item1.price = 1200
 item1.quantity = 5
-print(item1.calculate_total_price(item1.price, item1.quantity))
+
+item1 = Item('Laptop')
+item1.price = 1200
+item1.quantity = 5
