be79afc761bf648ff1e4cdc66aad32e869bba438
joyline-cheruto <joycheruto832@gmail.com>
2024-07-09 17:21:09 +0300

    refactor

--- codecamp.py
+++ codecamp.py
@@ -1,8 +1,8 @@
 class Item:
     def __init__(self, name, price, quantity):
         # validations
-        assert price >= 0
-        assert quantity >= 1
+        assert price >= 0, f"Price {price} is less than 0,enter value greater than 0."
+        assert quantity >= 1 ,f"Quantity{quantity} is less than 1,enter value greater than 1."
         # print(f"Instance created: {name}")
         self.name = name
         self.price = price
@@ -12,7 +12,7 @@ class Item:
         return self.price * self.quantity
 
 
-item1 = Item('phone', 1200, 5)
+item1 = Item('phone', 1200, -5)
 
 item2 = Item('Laptop', 2000, 3)
 print(item1.calculate_total_price())
