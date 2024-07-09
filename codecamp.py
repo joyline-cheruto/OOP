2e4ab4f9a5598ef527354b2566ba879cfd654e19
joyline-cheruto <joycheruto832@gmail.com>
2024-07-09 17:13:43 +0300

    validation using assert

--- codecamp.py
+++ codecamp.py
@@ -1,14 +1,18 @@
 class Item:
     def __init__(self, name, price, quantity):
-        print(f"Instance created: {name}")
+        # validations
+        assert price >= 0
+        assert quantity >= 1
+        # print(f"Instance created: {name}")
         self.name = name
         self.price = price
         self.quantity = quantity
 
-    def calculate_total_price(self, x, y):
-        return x * y
+    def calculate_total_price(self):
+        return self.price * self.quantity
 
 
 item1 = Item('phone', 1200, 5)
 
-item1 = Item('Laptop', 2000, 3)
+item2 = Item('Laptop', 2000, 3)
+print(item1.calculate_total_price())
