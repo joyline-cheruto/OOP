876cc3c7166e12ee7259a283a8d65157f838e0f6
joyline-cheruto <joycheruto832@gmail.com>
2024-07-15 14:48:26 +0300

    instantiate from class

--- codecamp.py
+++ codecamp.py
@@ -1,3 +1,4 @@
+import csv
 class Item:
     pay_rate = 0.8
     all = []
@@ -19,22 +20,19 @@ class Item:
     def apply_discount(self):
         self.price = self.price * self.pay_rate
 
+    @classmethod
+    def instantiate_from_class(cls):
+        with open('items.csv','r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+        for item in items:
+            print(item)
+
     def __repr__(self):
         return f"Item('{self.name}',{self.price},{self.quantity})"
         
 
 
-item1 = Item('phone', 1200, 5)
-item2 = Item('Laptop', 2000, 3)
-item3 = Item('printer', 2200, 2)
-item4 = Item('cable', 100, 10)
-item5 = Item('monitor', 500, 4)
 
-print(Item.__dict__)  #all attributes for Class level
-print(item2.__dict__)  #all attributes for instance level
-item1.apply_discount()
-print(item1.price)
-item5.pay_rate = 0.9
-item5.apply_discount()
-print(item5.price)
-print(Item.all)
+
+Item.instantiate_from_class()
